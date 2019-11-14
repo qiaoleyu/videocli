7a13c64268ff802cf8dbb56099d60274dae7ab5c
wangqingq <2423668851@qq.com>
2019-11-14 11:30:08 +0800

    弹幕页面

--- src/components/videoplay.vue
+++ src/components/videoplay.vue
@@ -414,6 +414,7 @@
   import ElButton from "../../node_modules/element-ui/packages/button/src/button";
   import ElInput from "../../node_modules/element-ui/packages/input/src/input";
   import $ from 'jquery';
+  import Cookies from 'js-cookie'
 
 
   //  弹幕
@@ -468,7 +469,11 @@
         },
 
 
-
+        name: "", // 昵称
+        websocket: null, // WebSocket对象
+        aisle: "", // 对方频道号
+        messageList: [], // 消息列表
+        messageValue: "", // 消息内容
 
 
       visible: false,
@@ -510,6 +515,9 @@
       }
     },
     mounted(){
+
+        Cookies.get("userId");
+
 //      var player = video('example-video');
 
       //      倍速播放
@@ -606,11 +614,16 @@
 
 
 
-
-
       // 播放回调
       onPlayerPlay(player) {
         console.log('player play!', player)
+        if (this.timer){
+          clearInterval(this.timer);
+        } else {
+          this.timer=setInterval(()=>{
+            this.toWebSocket();
+          },1000)
+       }
       },
 
       // 暂停回调
@@ -661,8 +674,82 @@
       //将侦听器绑定到组件的就绪状态。与事件监听器的不同之处在于，如果ready事件已经发生，它将立即触发该函数。。
       playerReadied(player) {
         console.log('example player 1 readied', player);
-      }
+      },
 
+      findBarrage:function () {
+        axios.get("api/findBarrage").then(res=>{
+          res.data();
+        })
+      },
+
+
+      conectWebSocket: function() {
+        console.log("建立连接");
+        if (this.name === "") {
+          this.$alert("请输入自己的昵称", "提示", {
+            confirmButtonText: "确定",
+            callback: action => {}
+          });
+        } else {
+          //判断当前浏览器是否支持WebSocket
+          if ("WebSocket" in window) {
+            this.websocket = new WebSocket(
+              "ws://localhost:8080/websocket/" + this.name
+            );
+          } else {
+            alert("不支持建立socket连接");
+          }
+          //连接发生错误的回调方法
+          this.websocket.onerror = function() {
+
+          };
+          //连接成功建立的回调方法
+          this.websocket.onopen = function(event) {
+
+          };
+          //接收到消息的回调方法
+          var that = this;
+          this.websocket.onmessage = function(event) {
+            var object = eval("(" + event.data + ")");
+            console.log(object);
+            if (object.type == 0) {
+              // 提示连接成功
+              console.log("连接成功");
+              that.showInfo(object.people, object.aisle);
+            }
+            if (object.type == 1) {
+              //显示消息
+              console.log("接受消息");
+              that.messageList.push(object);
+            }
+          };
+          //连接关闭的回调方法
+          this.websocket.onclose = function() {};
+          //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+          window.onbeforeunload = function() {
+            this.websocket.close();
+          };
+        }
+      },
+      // 发送消息
+      sendMessage: function() {
+        var socketMsg = { msg: this.messageValue, toUser: this.aisle };
+        if (this.aisle == "") {
+          //群聊.
+          socketMsg.type = 0;
+        } else {
+          //单聊.
+          socketMsg.type = 1;
+        }
+        this.websocket.send(JSON.stringify(socketMsg));
+      },
+      showInfo: function(people, aisle) {
+        this.$notify({
+          title: "当前在线人数：" + people,
+          message: "您的频道号：" + aisle,
+          duration: 0
+        });
+      }
 
     }
 }
